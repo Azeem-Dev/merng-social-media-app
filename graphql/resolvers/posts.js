7871f70e113e86ea68eec44345714e67123f06a5
Azeem Sharif <72242160+Azeem-Dev@users.noreply.github.com>
2022-01-04 22:22:05 +0500

    added new typeDefs and resolvers that helps us with comments and likes of a post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,10 +1,11 @@
+const { AuthenticationError, UserInputError } = require("apollo-server");
 const Post = require("../../models/post/Post");
 const checkAuth = require("../../utils/check-auth");
 module.exports = {
   Query: {
     async getPosts() {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
       } catch (e) {
         throw new Error(e);
@@ -34,6 +35,37 @@ module.exports = {
       });
       const post = await newPost.save();
       return post;
+    },
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.delete();
+          return "Post deleted Successfully";
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } catch (e) {
+        throw new Error(e);
+      }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
     },
   },
 };
